e57f002b394711a3b3b2e3bf85d1e813686ff8a1
Fabian Finke <4998526+fezu54@users.noreply.github.com>
2026-06-05 23:12:20 +0200

    fix(ble-service): Forumslader seem to be always connected (#5)

--- app/src/main/kotlin/org/happycode/karoo/forumslader/MainActivity.kt
+++ app/src/main/kotlin/org/happycode/karoo/forumslader/MainActivity.kt
@@ -1,8 +1,12 @@
 package org.happycode.karoo.forumslader
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.content.ContextCompat
 import org.happycode.karoo.forumslader.screens.MainScreen
 import org.happycode.karoo.forumslader.theme.AppTheme
 
@@ -10,10 +14,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        requestNeededPermissions()
+
         setContent {
             AppTheme {
                 MainScreen()
             }
+        }
+    }
+
+    private fun requestNeededPermissions() {
+        val permissions = mutableListOf<String>()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            permissions.add(Manifest.permission.BLUETOOTH_SCAN)
+            permissions.add(Manifest.permission.BLUETOOTH_CONNECT)
+        } else {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+            permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+
+        val missingPermissions = permissions.filter {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+
+        if (missingPermissions.isNotEmpty()) {
+            requestPermissions(missingPermissions.toTypedArray(), 101)
         }
     }
 }
--- app/src/main/kotlin/org/happycode/karoo/forumslader/model/ForumsladerBleProfile.kt
+++ app/src/main/kotlin/org/happycode/karoo/forumslader/model/ForumsladerBleProfile.kt
@@ -3,7 +3,8 @@ package org.happycode.karoo.forumslader.model
 import java.util.UUID
 
 object ForumsladerBleProfile {
-    val SERVICE_UUID: UUID = UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb")
+    val SERVICE_UUID_V5: UUID = UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb")
+    val SERVICE_UUID_V6: UUID = UUID.fromString("6e40ffe2-b5a3-f393-e0a9-e50e24dcca9e")
     val CHARACTERISTIC_UART_TX_RX: UUID = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb")
     val CLIENT_CHARACTERISTIC_CONFIGURATION_DESCRIPTOR: UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
 }
